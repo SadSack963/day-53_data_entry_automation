08e0fc3064a4adbcea953a08001792cd92205017
john <john@jpatmore.plus.com>
2021-03-08 21:56:15 +0000

    Code refactored. BeautifulSoup retrieves the Zillow page.

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ from bs4 import BeautifulSoup
 from selenium import webdriver, common
 from selenium.webdriver.common.keys import Keys
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # pip install python-dotenv
 from time import sleep
 
-chrome_driver_path = "E:/Python/WebDriver/chromedriver.exe"
-firefox_driver_path = "E:/Python/WebDriver/geckodriver.exe"
-opera_driver_path = "E:/Python/WebDriver/operadriver.exe"
-
-load_dotenv("E:/Python/EnvironmentVariables/.env")
 
 FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLScHVLX-4qmgXvVSB7nebMtvHCyO5ncn63-y-AES0BM602cIjQ/viewform" \
            "?usp=sf_link"
+
 ZILLOW_URL = 'https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState={' \
              '"pagination":{},' \
              '"usersSearchTerm":null,' \
@@ -41,20 +37,55 @@ ZILLOW_URL = 'https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState={'
              '"mapZoom":12' \
              '}'
 
+WEB_FILE = "./data/zillow.html"
 
-class Zillow:
-    def __init__(self, browser: str = "chrome"):
-        b = browser.lower()
-        if b == "chrome":
-            self.driver = webdriver.Chrome(executable_path=chrome_driver_path)
-        elif b == "firefox":
-            self.driver = webdriver.Firefox(executable_path=firefox_driver_path)
-        elif b == "opera":
-            self.driver = webdriver.Opera(executable_path=opera_driver_path)
+
+# Use Selenium to fill in the form you created
+
+class Form:
+    def __init__(self, url, browser: str = "chrome"):
+        browser = browser.lower()
+        # Declare variables
+        self.chrome_driver_path = self.firefox_driver_path = self.opera_driver_path = ""
+        # self.driver = webdriver.Chrome()  # Windows error: 'chromedriver' executable needs to be in PATH.
+
+        # Get the path to the WebDriver and environment variables
+        #   depending upon the operating system
+        self.get_os_path()
+        self.get_driver(browser)
+        self.driver.get(url)
+
+    def get_os_path(self):
+        # Detect Operating System and set paths to local files
+        import platform
+        if platform.system() == "Windows":
+            self.chrome_driver_path = "E:/Python/WebDriver/chromedriver.exe"
+            self.firefox_driver_path = "E:/Python/WebDriver/geckodriver.exe"
+            self.opera_driver_path = "E:/Python/WebDriver/operadriver.exe"
+            load_dotenv("E:/Python/EnvironmentVariables/.env")
+        elif platform.system() == "Linux":
+            # (Set WebDriver file permissions to 755)
+            # -rwxr-xr-x 1 john john 11755976 Jan 27 03:32 chromedriver
+            # -rwxr-xr-x 1 john john  7965656 Jan 14 08:51 geckodriver
+            # -rwxr-xr-x 1 john john 14990832 Feb  3 10:29 operadriver
+            self.chrome_driver_path = "/home/john/Development/Python/WebDriver/chromedriver"
+            self.firefox_driver_path = "/home/john/Development/Python//WebDriver/geckodriver"
+            self.opera_driver_path = "/home/john/Development/Python/WebDriver/operadriver"
+            load_dotenv("/media/sf_Python/EnvironmentVariables/.env")
         else:
-            print(f"Invalid browser \"{browser}\".\nOnly Chrome, Firefox and Opera are configured.")
+            print("OS not supported!")
             exit()
-        self.driver.get(ZILLOW_URL)
+
+    def get_driver(self, b):
+        if b == "chrome":
+            self.driver = webdriver.Chrome(executable_path=self.chrome_driver_path)
+        elif b == "firefox":
+            self.driver = webdriver.Firefox(executable_path=self.firefox_driver_path)
+        elif b == "opera":
+            self.driver = webdriver.Opera(executable_path=self.opera_driver_path)
+        else:
+            print(f"Invalid browser \"{b}\".\nOnly Chrome, Firefox and Opera are configured.")
+            exit()
 
     def find_elements(self, method: str, specifier: str):
         """
@@ -75,6 +106,7 @@ class Zillow:
         :return:  list of elements found
         """
         by = method.lower()
+        list_elements = []
         for _ in range(10):
             try:
                 if by == "id":
@@ -106,4 +138,67 @@ class Zillow:
         print(f"Unable to find any element by \"{by}\" with \"{specifier}\"")
 
 
-zillow = Zillow("opera")
+# Use BeautifulSoup/Requests to scrape all the listings from the Zillow web address
+
+def get_web_page(file, url):
+    """
+    Retrieve requested web page\n
+    Use library render() to execute the page's JavaScript\n
+    Save the rendered web page to web file
+
+    :param file: file where web page is saved
+    :param url: URL of web page to retrieve
+    :return: nothing
+    """
+    from requests_html import HTMLSession
+
+    # Create an HTML Session object
+    session = HTMLSession()
+    # Use the object above to connect to needed webpage
+    response = session.get(url)
+    # Run JavaScript code on webpage
+    response.html.render()
+
+    # Save web page to file
+    # print("Saving to file")
+    with open(file, mode="w", encoding="utf-8") as fp:
+        fp.write(response.html.html)
+
+
+def read_web_file(file, url):
+    """
+    If web file does not exist, then retrieve web page\n
+    Open web file and return a BeautifulSoup object (HTML)
+
+    :param file:  file where web page is saved
+    :param url:  URL of web page to retrieve
+    :return: HTML soup
+    """
+    try:
+        open(file)
+    except FileNotFoundError:
+        get_web_page(file, url)
+    else:
+        pass
+    finally:
+        # Read the web page from file
+        # print("Reading from file")
+        with open(file, mode="r", encoding="utf-8") as fp:
+            content = fp.read()
+        return BeautifulSoup(content, "html.parser")
+
+
+if __name__ == "__main__":
+    # Use BeautifulSoup to retrieve the Zillow web page
+    soup = read_web_file(file=WEB_FILE, url=ZILLOW_URL)
+    # print(f"result = {soup}")
+
+    # TODO Create a list of links for all the listings you scraped.
+
+    # TODO Create a list of prices for all the listings you scraped.
+
+    # TODO Create a list of addresses for all the listings you scraped.
+
+    # TODO Use selenium to fill in the Google Form "San Francisco Renting"
+    # form = Form(url=FORM_URL, browser="opera")
+
